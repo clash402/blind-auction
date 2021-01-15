105199247195db11ac46799fa6dfc089c015ba41
Josh Courtney <joshrcourtney@gmail.com>
2021-01-15 12:19:22 -0600

    Refactor main.py

--- main.py
+++ main.py
@@ -1,41 +1,38 @@
-from art import logo
 import os
 
-# PROPERTIES
-bids = {}
+from art import logo
 
 
 # METHODS
-def find_highest_bid():
-	highest_bidder = ""
-	highest_bid = 0
+def find_highest_bid(final_bids):
+    highest_bidder = ""
+    highest_bid = 0
 
-	for key, value in sorted(bids.items()):
-		if value > highest_bid:
-			highest_bidder = key
-			highest_bid = value
+    for key, value in sorted(final_bids.items()):
+        if value > highest_bid:
+            highest_bidder = key
+            highest_bid = value
 
-	return f"The highest bidder is {highest_bidder} with ${highest_bid}."
+    return f"The highest bidder is {highest_bidder} with ${highest_bid}."
 
 
 # MAIN
 print(logo)
 print("Welcome to the secret auction.")
 
+bids = {}
 bidding_is_in_progress = True
 
 while bidding_is_in_progress:
-	name = input("What's your name? ")
-	bid = int(input("What's your bid? $"))
-	will_repeat = input("Are there any other bidders? (y/n) ").lower()
+    name = input("What's your name? ")
+    bid = int(input("What's your bid? $"))
+    bids[name] = bid
 
-	bids[name] = bid
+    if input("Are there any other bidders? (y/n) ").lower() == "y":
+        os.system('cls' if os.name == 'nt' else 'clear')
+    else:
+        bidding_is_in_progress = False
 
-	if will_repeat == "y":
-		os.system('cls' if os.name == 'nt' else 'clear')
-	else:
-		bidding_is_in_progress = False
-
-winner = find_highest_bid()
+winner = find_highest_bid(bids)
 
 print(winner)
